f2357968f534ee5a5d3e580b3e537bfeba2912b3
williamking5 <liuyongbin.ouc@gmail.com>
2016-03-29 21:13:31 +0800

    3.29

--- src/send.h
+++ src/send.h
@@ -1,25 +1,37 @@
 int Send(char lockname[],char username[]){
-	char filename[100];
+	char putname[100],filename[100],*pp;
 	printf("请输入文件名： ");
 	scanf("%s",filename);
+	
+	pp=putname;
+	strcpy(putname,filename);
+	if (putname[0]=='\''&&putname[strlen(putname)-1]=='\''){
+		putname[strlen(putname)-1]=0;
+		pp++;
+	}
 
-	FILE *fp=fopen(filename,"r");
+	FILE *fp=fopen(pp,"r");
 	if (fp==NULL) {
 		printf("没有找到%s,您要创建该文件吗？  y/n\n",filename);
 		char tmp[3];
 		scanf("%s",tmp);
 		if (!strcmp(tmp,"n")) return 0;
 
-		fp=fopen(filename,"w");
+		fp=fopen(pp,"w");
 		fclose(fp);
 		char cmd[200];
-		sprintf(cmd,"gedit %s",filename);
+		sprintf(cmd,"gedit %s",pp);
 		system(cmd);
 	}
 	else fclose(fp);
 
+	while (strstr(pp,"/")!=NULL){
+		int k=strstr(pp,"/")-pp;
+		pp+=k+1;
+	}
+
 	char cmd[200],nowtime[20];
 	gettime(nowtime);
-	sprintf(cmd,"./qshell fput %s %s-%s-%s-%s %s",BUCKET,lockname,nowtime,filename,username,filename);
+	sprintf(cmd,"./qshell fput %s %s-%s-%s-%s %s",BUCKET,lockname,nowtime,pp,username,filename);
 	system(cmd);
 }
